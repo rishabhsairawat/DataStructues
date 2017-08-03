8708e41e3c045a9b486addf5707f96e06657337c
Rishabh Sairawat <rishabh.sairawat01@gmail.com>
2017-08-04 01:05:16 +0530

    Update LeftViewOfABinaryTree.java

--- LeftViewOfABinaryTree.java
+++ LeftViewOfABinaryTree.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeftViewOfABinaryTree {
 	Node root;
 	static int max_level=0;
@@ -29,11 +32,46 @@ public class LeftViewOfABinaryTree {
 		getLeftView(node.left,level+1);
 		getLeftView(node.right,level+1);
 	}
+	void leftView2(Node node){
+		if(node==null)
+			return;
+		Queue<Node> q=new LinkedList<Node>();
+		q.add(node);
+		q.add(null);
+		boolean isFirst=true;
+		while(!q.isEmpty()){
+			Node temp=q.peek();
+			q.poll();
+			if(isFirst&&temp!=null){
+				System.out.print(temp.key+" ");
+				if(temp.left!=null)
+					q.add(temp.left);
+				if(temp.right!=null)
+					q.add(temp.right);
+				isFirst=false;
+			}
+			else if(temp==null && q.size()>1){
+				isFirst=true;
+				q.add(null);
+			}
+			else if(temp!=null){
+				if(temp.left!=null)
+					q.add(temp.left);
+				if(temp.right!=null)
+					q.add(temp.right);
+			}
+		}
+	}
+	
+		
+	
 	public static void main(String[] args) {
 		LeftViewOfABinaryTree tree=new LeftViewOfABinaryTree();
 		tree.makeTree();
 		System.out.println("Left View is: ");
 		tree.getLeftView(tree.root, 1);
+		System.out.println("\nLeft View without using recursion is: ");
+		tree.leftView2(tree.root);
 	}
 	
 
